ae54c38457fa62187c37c4c28f7eb80d06f19302
Paula_Dev <paulasuyaneprojeto2023@gmail.com>
2023-11-07 15:15:03 -0300

    finalizada

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 function inserindoNumeros(numeros){
 var num = document.getElementById("result").innerHTML 
-document.getElementById("result").innerHTML = numeros + num
+document.getElementById("result").innerHTML =  num + numeros
+}
+
+function limpar(){
+    document.getElementById('result').innerHTML = ' '
+}
+
+function apagarDigito(){
+    var result = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = result.substring(0, result.length -1)
+}
+function calculo(){
+    var result = document.getElementById('result').innerHTML;
+    if(result){
+     document.getElementById('result').innerHTML = eval(result)
+    }else{
+      window.alert('Insira algum valor')
+    }
 }
